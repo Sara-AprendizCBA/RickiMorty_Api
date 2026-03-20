8adb9eebc03f8467c297d4a6ca096e8558b8194e
V <pvalery748@gmail.com>
2026-03-20 14:46:47 -0500

    Update character_provider.dart

--- lib/providers/character_provider.dart
+++ lib/providers/character_provider.dart
@@ -1,1 +1,41 @@
- // character_provider
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:rickandmortyapp/models/character_mode.dart';
+
+class CharacterProvider with ChangeNotifier {
+  List<Character> _favorites = [];
+
+  List<Character> get favorites => _favorites;
+
+  CharacterProvider() {
+    _loadFavorites();
+  }
+
+  bool isFavorite(int characterId) {
+    return _favorites.any((c) => c.id == characterId);
+  }
+
+  void toggleFavorite(Character character) {
+    if (isFavorite(character.id!)) {
+      _favorites.removeWhere((c) => c.id == character.id);
+    } else {
+      _favorites.add(character);
+    }
+    _saveFavorites();
+    notifyListeners();
+  }
+
+  Future<void> _saveFavorites() async {
+    final prefs = await SharedPreferences.getInstance();
+    final list = _favorites.map((c) => jsonEncode(c.toJson())).toList();
+    await prefs.setStringList('favorites', list);
+  }
+
+  Future<void> _loadFavorites() async {
+    final prefs = await SharedPreferences.getInstance();
+    final list = prefs.getStringList('favorites') ?? [];
+    _favorites = list.map((s) => Character.fromJson(jsonDecode(s))).toList();
+    notifyListeners();
+  }
+}  
